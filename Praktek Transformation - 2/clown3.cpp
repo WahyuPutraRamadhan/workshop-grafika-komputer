399c5cbe13d6334dc474463c0376fcea0d74576e
WahyuPutraRamadhan <wahyuputraramadhan12@gmail.com>
2022-09-28 11:26:15 +0700

    Update clown3.cpp

--- Praktek Transformation - 2/clown3.cpp
+++ Praktek Transformation - 2/clown3.cpp
@@ -131,7 +131,7 @@ void drawScene(void)
 
 	//mata kiri
 	glPushMatrix();
-	glColor3f(0.0,1.0,0.0);
+	glColor3f(0.0,0.0,0.0);
 	glTranslatef(0.0,1.0,0.0);
 	glScalef(2.5,0.0,0.0);
 	glRotated(90,1.0,0.0,0.0);
